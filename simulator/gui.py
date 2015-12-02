1c82a21845e3868cabdc6f3d9bffce2306eeaa63
Pawel Jakubowski <pawel-jakubowski@hotmail.com>
2015-12-02 23:31:14 +0100

    Initial gui window

---
+++ simulator/gui.py
@@ -0,0 +1,8 @@
+import Tkinter as tk
+
+class Window:
+    title = "Mapping system - simulator"
+
+    def __init__(self, root):
+        self.root = root
+        root.title(self.title)
--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,5 +1,10 @@
+import Tkinter as tk
+import gui
+
 def main():
-    print "Hello world!"
+    rootFrame = tk.Tk()
+    app = gui.Window(rootFrame)
+    rootFrame.mainloop()
 
 if __name__ == '__main__':
     main()  # This is executed if file is not imported
