d5d5d9d7eeb6773bc31da337c491ca8031072b58
Pawel Jakubowski <pawel-jakubowski@hotmail.com>
2015-12-11 13:58:30 +0100

    Add simple robots animation

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,10 +1,23 @@
 import Tkinter as tk
 import gui
 
+
 def main():
+    gui, app = initGui()
+    time = 1
+    for robot in app.board.robots:
+        gui.after(time, robot.animate, gui, time)
+    gui.mainloop()
+
+
+def initGui():
     rootFrame = tk.Tk()
     app = gui.Window(rootFrame)
-    rootFrame.mainloop()
+    print "Board size: %d" % app.board.size
+    print "Robots: %d" % app.board.robots_count
+    print "Robots coordinates:"
+    print app.board.robots
+    return rootFrame, app
 
 if __name__ == '__main__':
     main()  # This is executed if file is not imported
