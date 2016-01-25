b597f5d7f802feb5b150b1fa5db936f53ad047b9
przem <sendthenote@gmail.com>
2016-01-25 19:29:39 +0100

    Manually merged with Skora.

--- przem_controller.py
+++ przem_controller.py
@@ -1,6 +1,5 @@
 import zmq
 import time
-from requests.utils import get_environ_proxies
 
 import communication_pb2 as com
 
@@ -44,52 +43,108 @@ class Controller:
         self.stateMatrix = [[0 for x in range(env.x)] for x in range(env.y)]
 
 
-    def generatePath(self, robotID):
+    def generatePath(self,robotID,xMax,yMax,base):
         # place for magic path generator
-        # temp path
-        # path0 = [[5,5], [5,4], [5,3], [5,2], [5,1], [5,0], [6,0]]
-        # path1 = [[6,5], [7,5], [8,5], [9,5], [8,5], [7,5], [6,5]]
-        path0 = [[5,5], [5,4], [6,4], [6,3], [5,3], [5,2], [6,2], [6,1], [5,1], [5,0], [6,0]]
-        path1 = [[6,5], [6,4], [6,3], [6,2], [6,1], [6,0], [7,0]]
+        path_temp=[]
+        n=robotID
+        # for now just spirale around whole map
+        for i in range(0,base[1]+1-n):  #czyli od punktu bazowego do zera
+            path_temp.append([base[0],base[1]-i]) #x sie nie zmienia
 
-        # path0 = [[7,10], [7,11], [7,12], [7,13], [7,14], [8,14], [9,14], [10,14], [10,15], [10,16]]
-        # path1 = [[8,10], [8,11], [8,12], [8,13], [8,14], [8,15], [8,16]]
+        lastX=path_temp[-1][0]
+        lastY=path_temp[-1][1]
+        for i in range (1,(xMax)-path_temp[-1][0]-n):
+            path_temp.append([lastX+i,lastY])
 
+        lastX=path_temp[-1][0]
+        lastY=path_temp[-1][1]
+        for i in range (1,yMax-path_temp[-1][1]-n):
+            path_temp.append([lastX,lastY+i])
 
-        if robotID == 0:
-            tempPath = {'robotID': robotID, 'path':path0}
-        else:
-            tempPath = {'robotID': robotID, 'path':path1}
+        lastX=path_temp[-1][0]
+        lastY=path_temp[-1][1]
+        for i in range (1,xMax-2*n):
+            path_temp.append([lastX-i,lastY])
 
+        lastX=path_temp[-1][0]
+        lastY=path_temp[-1][1]
+        for i in range (1,yMax-2*n):
+            path_temp.append([lastX,lastY-i])
+
+        lastX=path_temp[-1][0]
+        lastY=path_temp[-1][1]
+        for i in range (1,1+base[0]-n):
+            path_temp.append([lastX+i,lastY])
+
+        lastX=path_temp[-1][0]
+        lastY=path_temp[-1][1]
+        for i in range (1,1+base[1]-n):
+            path_temp.append([lastX,lastY+i])
+
+        if robotID == n_robots-1:
+            path_temp.append(base)
+            for z in range(0,base[0]-(base[0]-n_robots)+1):
+                for i in range(1,yMax-base[1]):  #czyli od punktu bazowego do samego dolu
+                    path_temp.append([path_temp[-1][0],path_temp[-1][1]+1]) #x sie nie zmienia
+                path_temp.append([path_temp[-1][0]-1,path_temp[-1][1]]) #x sie nie zmienia
+                for i in range(0,yMax-base[1]-1):  #czyli od punktu bazowego do samego dolu
+                    path_temp.append([path_temp[-1][0],path_temp[-1][1]-1]) #x sie nie zmienia
+                for i in range(0,yMax-path_temp[-1][1]):
+                    path_temp.append([path_temp[-1][0],path_temp[-1][1]-1]) #x sie nie zmienia
+                for i in range(0,base[0]-path_temp[-1][0]):
+                    path_temp.append([path_temp[-1][0]+1,path_temp[-1][1]]) #x sie nie zmienia
+                for i in range(0,base[1]-path_temp[-1][1]):
+                    path_temp.append([path_temp[-1][0],path_temp[-1][1]+1]) #x sie nie zmienia
+
+        tempPath = {'robotID': robotID, 'path':path_temp}
         # remember path in robotsPaths dict!
         self.robotsPaths.append(tempPath)
-        if robotID == 0:
-            return path0
-        else:
-            return path1
+        return path_temp
 
 
     def getCoordFromPath(self, robotID, stage):
         # robotID-1 because index list starts from 0, we are num. robots from 1
         id = self.robotsPaths[robotID]['robotID']
         path = self.robotsPaths[robotID]['path']
+
         x = path[stage][0]
         y = path[stage][1]
         return x,y
 
 
+    def getNumOfStages(self, robotID):
+        # we are conting stages from 0
+        nStages = len(self.robotsPaths[robotID]['path']) - 1
+        return nStages
+
+
+    def isThisLastStage(self, dEvent):
+        if dEvent['currentStage'] == self.getNumOfStages(dEvent['robotID']):
+            return True
+        else:
+            return False
+
+
     def checkIfFuturePosIsFree(self, dEvent):
+        currentX, currentY = self.getCoordFromPath(dEvent['robotID'], dEvent['currentStage'])
         futureX, futureY = self.getCoordFromPath(dEvent['robotID'], dEvent['futureStage'])
+
         if self.stateMatrix[futureY][futureX] == 0:
             return True
         else:
+            print("R{0} jestem w: {1} {2} - current".format(dEvent['robotID'], currentX, currentY))
             print("R{0} {1} {2} resource is occupied!".format(dEvent['robotID'], futureX, futureY))
             return False
 
+
     def takeFutureResorce(self, dEvent):
         futureX, futureY = self.getCoordFromPath(dEvent['robotID'], dEvent['futureStage'])
+        currentX, currentY = self.getCoordFromPath(dEvent['robotID'], dEvent['currentStage'])
         self.stateMatrix[futureY][futureX] = 1
+
+        print("R{0} jestem w: {1} {2} - current".format(dEvent['robotID'], currentX, currentY))
         print("R{0} zajmuje: {1} {2} - future".format(dEvent['robotID'], futureX, futureY))
+
 
     def freeOldResource(self, dEvent):
         oldX, oldY = self.getCoordFromPath(dEvent['robotID'], dEvent['oldStage'])
@@ -99,45 +154,6 @@ class Controller:
         if oldStage >= 0:
             self.stateMatrix[oldY][oldX] = 0
             print("R{0} zwalniam: {1} {2}".format(dEvent['robotID'], oldX, oldY))
-
-    def takeCurrentResource(self, dEvent):
-        currentX, currentY = self.getCoordFromPath(dEvent['robotID'], dEvent['currentStage'])
-        self.stateMatrix[currentY][currentX] = 1
-        print("R{0} zajmuje: {1} {2} - current".format(dEvent['robotID'], currentX, currentY))
-
-
-
-    # def updateState(self, dEvent):
-    #     currentX, currentY = self.getCoordFromPath(dEvent['robotID'], dEvent['currentStage'])
-    #     futureX, futureY = self.getCoordFromPath(dEvent['robotID'], dEvent['futureStage'])
-    #     oldX, oldY = self.getCoordFromPath(dEvent['robotID'], dEvent['oldStage'])
-    #     oldStage = dEvent['oldStage']
-    #
-    #     print(self.stateMatrix)
-    #     print("R{0} jestem w : {1} {2}".format(dEvent['robotID'], currentX, currentY))
-    #
-    #     # free old resource
-    #     if oldStage >= 0:
-    #         self.stateMatrix[oldY][oldX] = 0
-    #         print("R{0} zwalniam: {1} {2}".format(dEvent['robotID'], oldX, oldY))
-
-    # def checkIfFuturePosIsFree(self, dEvent):
-    #     currentX, currentY = self.getCoordFromPath(dEvent['robotID'], dEvent['currentStage'])
-    #     futureX, futureY = self.getCoordFromPath(dEvent['robotID'], dEvent['futureStage'])
-    #     oldX, oldY = self.getCoordFromPath(dEvent['robotID'], dEvent['oldStage'])
-    #     oldStage = dEvent['oldStage']
-    #
-    #     # check if future stage is free
-    #     if self.stateMatrix[futureY][futureX] == 0:
-    #         # take new resource
-    #         self.stateMatrix[futureY][futureX] = 1
-    #         print("R{0} zajmuje: {1} {2}".format(dEvent['robotID'], futureX, futureY))
-    #         return True
-    #
-    #     # resource is occupied
-    #     else:
-    #         print("R{0} {1} {2} resource is occupied!".format(dEvent['robotID'], futureX, futureY))
-    #         return False
 
 
     def defineRobot(self, robotId, robotX0, robotY0, robotPath):#define how many robots should be created
@@ -168,7 +184,6 @@ class Controller:
         print("Receive Obs event ze:")
         print(dEvent)
 
-        self.takeCurrentResource(dEvent)
         self.freeOldResource(dEvent)
 
         self.notHandledEvents.append(event)
@@ -196,17 +211,16 @@ class Controller:
             self.waitForRecvEvent = True
 
         print("+++")
-        self.takeCurrentResource(dEvent)
-        # self.freeOldResource(dEvent)
-        # if resource is free, we are able to handle this event
-        if self.checkIfFuturePosIsFree(dEvent) == True:
-            self.takeFutureResorce(dEvent)
-            self.sendContEvent(dEvent['robotID'], dEvent['futureStage'])
-        else:
-            # put this resource in highPrioEvents
-            self.highPrioEvents.append(dEvent)
-            # print("highPro: "+str(self.highPrioEvents))
-        print("---")
+        if self.isThisLastStage(dEvent) == False:
+            # if resource is free, we are able to handle this event
+            if self.checkIfFuturePosIsFree(dEvent) == True:
+                self.takeFutureResorce(dEvent)
+                self.sendContEvent(dEvent['robotID'], dEvent['futureStage'])
+            else:
+                # put this resource in highPrioEvents
+                self.highPrioEvents.append(dEvent)
+                # print("highPro: "+str(self.highPrioEvents))
+            print("---")
 
 
     def allHighPrioEventsChecked(self):
@@ -279,36 +293,31 @@ class Controller:
     def sendEnvMsg(self):
         # ta wiadmosc wysyalana jest tylko jeden raz
         # wysylamy sciezki kazdego robota na poczatku do symulatora
+
+        global n_robots #total number of robots [will use it in path generation] could be a global?
+        n_robots=6
         print "sending the initialize environment/path message"
         time.sleep(1)
-        # self.defineEnv(20,20,7,10)
-        self.defineEnv(10,10,5,6)
-        time.sleep(1)
-        self.defineRobot(0,5,5, self.generatePath(0))
-        time.sleep(1)
-        self.defineRobot(1,6,5, self.generatePath(1))
-        time.sleep(1)
-        # self.defineRobot(3,6,5, self.generatePath())
-        # time.sleep(1)
+        self.defineEnv(20,20,7,10)
+        for n in range(0,n_robots):
+            time.sleep(1)
+            #robot ID xMax yMax robot_base_point
+            self.defineRobot(n,7+n,10, self.generatePath(n,20,20,[7+n,10]))
 
-	 
+
+
 def main():
     controller = Controller()
     time.sleep(1)
     controller.sendEnvMsg()
     time.sleep(5)
 
-    # controller.sendContEvent(1,1)
-    # controller.sendContEvent(2,1)
-
     while True:
-            # print("1")
             if controller.waitForRecvEvent == True:
                 controller.receiveEvents()
-            # print("2")
+
             controller.handleEvents()
-            # print("3")
+
 
 if __name__ == '__main__':
     main()  # This is executed if file is not imported
-
